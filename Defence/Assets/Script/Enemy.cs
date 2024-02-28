73a9822e7e5ab67decaacd35a3c6c73e5b3efbca
SoChanYoung <james060702@gmail.com>
2024-02-28 12:53:16 +0900

    1일차 과제완료

--- Defence/Assets/Script/Enemy.cs
+++ Defence/Assets/Script/Enemy.cs
@@ -2,70 +2,69 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Enemy : MonoBehaviour // MonoBehaviour을 상속받는 Enemy 클래스
 {
-    private GameObject _currentWayPoint;
-    private int _wayPointCount = 0;
-    private Vector3 _moveDirection = Vector3.zero;
-    private int _hp = 5;
+    private GameObject _currentWayPoint; // GameObject 타입의 변수를 선언, 현재의 방향 포인트로서의 역활을 한다
+    private int _wayPointCount = 0; // int 타입의 변수를 선언 및 정의,  방향포인트의 개수를 새는 역활을 한다.
+    private Vector3 _moveDirection = Vector3.zero; // Vector3 타입의 변수를 선언 및 정의,  해당 클래스를 갖고 있는 오브젝트의 이동방향 역활을 하는 변수
+    private int _hp = 5; // int 타입의 변수를 선언 밎 정의, 해당 Enemy의 체력값 역활을 한다
 
-    [HideInInspector]
-    public GameObject[] WayPoints;
-    public int MaxHp = 5;
-    public float MoveSpeed = 10;
-    public int StealCoin = 100;
-    public int Damage = 1;
+    [HideInInspector] // 인스펙터 창에서 변수들을 보이지 않게 하기 위해 사용
+    public GameObject[] WayPoints; // 방향을 바꿔줄떄 사용되는 게임오브젝트로서 게임오브젝트 타입의 배열
+    public int MaxHp = 5; // Enemy의 최대 체력값으로서의 역활을 하는 int 타입의 변수
+    public float MoveSpeed = 10; // Enemy의 이동속도 값의 역활을 하는 float 타입의 변수
+    public int StealCoin = 100; // Enemy를 처치 했을때 얻는 코인의 값 역활을 하는 int 타입의 변수를 선언 밎 정의
+    public int Damage = 1; // Enemy가 주는 데미지 값이 역활을 하는 int 타입의 변수 
 
-    private void Start()
+    private void Start() // 씬이 시작됬을때 작동되는 함수
     {
-        _hp = MaxHp;
-        _currentWayPoint = WayPoints[0];
-        SetRotationByDirection();
+        _hp = MaxHp; // 체력값에 최대 체력값을 넣어준다
+        _currentWayPoint = WayPoints[0]; // 현재 방향 포인트 값에 WayPoints 배열의 0번째 값을 넣어준다
+        SetRotationByDirection(); // SetRotationByDirection 함수를 호출한다
     }
 
-    private void Update()
+    private void Update() // 매 프레임마다 작동되는 함수
     {
-        transform.position += _moveDirection * MoveSpeed * Time.deltaTime;
+        transform.position += _moveDirection * MoveSpeed * Time.deltaTime; // 오브젝트에 위치값에 (이동방향 * 이동속도 * 시간)값을 더해줘 오브젝트가 움직일 수 있도록 해준다
+        Vector3 TargetPosition = _currentWayPoint.transform.position; // Vector3 변수에 현재 방향점의 위치값을 넣는다
+        TargetPosition.y = transform.position.y; // TargetPostion.y 값에 현재 Enemy 클래스 오브젝트의 위치 y값을 넣는다
 
-        Vector3 TargetPosition = _currentWayPoint.transform.position;
-        TargetPosition.y = transform.position.y;
-
-        if (Vector3.Distance(transform.position, TargetPosition) <= 0.02f)
+        if (Vector3.Distance(transform.position, TargetPosition) <= 0.02f) // Vector3.Distance를 통해 현재 오브젝트의 위치값과, TargetPosition의 위치와의 거리를 계산하여 0.02f보다 작거나 같다면 아래 코드를 작동
         {
-            if (_wayPointCount >= WayPoints.Length - 1)
+            if (_wayPointCount >= WayPoints.Length - 1) // 만약 wayPointCount 방향점 계수 값이, WayPoints 배열의 길이 값에 -1을 더한 값보다 적다 크거나 갔다면 아래 코드를 작동
             {
                 //GameManager.Inst.playerCharacter.Damaged(Damage);
-                Destroy(gameObject);
-                return;
+                Destroy(gameObject); // 자기 자신 오브젝트를 삭제
+                return; // 반환한다
             }
 
-            _wayPointCount = Mathf.Clamp(_wayPointCount + 1, 0, WayPoints.Length);
-            _currentWayPoint = WayPoints[_wayPointCount];
+            _wayPointCount = Mathf.Clamp(_wayPointCount + 1, 0, WayPoints.Length); //방향점 카운트 값에 Mathf.Clamp 함수를 통해 방향점 값에 1을 더해주고 최소값을 0으로 최댓값을 WayPoints배열의 길이값으로 하여 해당 값 안을 벗어나지 않도록 함
+            _currentWayPoint = WayPoints[_wayPointCount]; // 현재 방향점 값에 WayPoints배열의 _WayPointCount번쨰 칸 배열의 값을 넣음
 
-            SetRotationByDirection();
+            SetRotationByDirection(); // SetRotationByDirection 함수를 호출한다
         }
     }
 
-    private void SetRotationByDirection()
+    private void SetRotationByDirection() // SetRotationByDirection 함수를 선언 밎 정의,  해당 함수는 
     {
-        _moveDirection = _currentWayPoint.transform.position - transform.position;
-        _moveDirection.y = 0;
-        _moveDirection.Normalize();
+        _moveDirection = _currentWayPoint.transform.position - transform.position; // 이동방향을 현재방향점의 위치값에서 - 자신의 위치값을 뺀것으로 하며 자기 자신을 이동시킨다
+        _moveDirection.y = 0; // _moveDirection의 y값을 0으로 해준다
+        _moveDirection.Normalize(); // 값을 Normalize 함수를 통해 정규화 시켜준다
 
-        transform.rotation = Quaternion.LookRotation(_moveDirection, Vector3.up);
+        transform.rotation = Quaternion.LookRotation(_moveDirection, Vector3.up); // 현재 오브젝트의 회전값에 LookRotation함수를 통해 매개변수 _moveDirection을 회전시킬 방향 백터로 하고 방향 벡터를 바라보고 있을 떄의 위를 지정한다
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other) // Trigger가 닿았을떄 작동되는 함수 
     {
-        if (other.CompareTag("Projectile"))
+        if (other.CompareTag("Projectile")) // 만약 "Projectile"태그인 collider에 닿았다면 아래 코드 작동
         {
-            _hp = Mathf.Clamp(_hp - 1, 0, MaxHp);
+            _hp = Mathf.Clamp(_hp - 1, 0, MaxHp); // 체력값을 최소값 0과 최댓값 최대체력값에 벗어나지 않는 선에서 체력을 -1 한다
 
-            if (_hp <= 0)
+            if (_hp <= 0) // 만약 체력이 0보다 작거나 같다면
             {
-                gameObject.SetActive(false);
-                GameManager.Inst.EnemyDead(StealCoin);
-                Destroy(gameObject);
+                gameObject.SetActive(false); // 게임 오브젝트를 SetActive(false)하여 씬에서 보이지 않도록 하고 작동을 멈춘다
+                GameManager.Inst.EnemyDead(StealCoin); // 게임 메니저에서 EnemyDead함수를 작동시키고 매개면수값인 stealCoin값을 얻는다 
+                Destroy(gameObject); //해당 오브젝트를 삭제
             }
         }
     }
--- Defence/Assets/Script/EnemySpawner.cs
+++ Defence/Assets/Script/EnemySpawner.cs
@@ -2,41 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemySpawner : MonoBehaviour
-{
-    public Transform SpawnPosition;
-    public GameObject[] WayPoints;
-    public GameObject EnemyPrefab;
-    public float SpawnCycleTime = 1f;
+public class EnemySpawner : MonoBehaviour //MomoBehaviou을 상속받는 클래스 EnmeySpanwer
+{   
+    public Transform SpawnPosition; // Enemy 오브젝트를 생성할 위치를 정하는 Transform 타입의 변수
+    public GameObject[] WayPoints;  // 방향을 바꿔줄떄 사용되는 게임오브젝트로서 게임오브젝트 타입의 배열
+    public GameObject EnemyPrefab;  // EnemySpawner가 소환시킬 Enemy 프리팹
+    public float SpawnCycleTime = 1f; //실수 타입의 적의 소환 사이클의 딜레이 시간을 담당하는 변수
 
-    private bool _bCanSpawn = true;
+    private bool _bCanSpawn = true; // 논리형 타입의 변수로서 적을 소환할 것인지에 대한 참, 거짓 여부를 결정짔는 역활을 한다
 
-    private void Start()
+    private void Start() // 씬이 시작될때 작동되는 함수
     {
-        Activate();
+        Activate(); // 함수 Active 호출 
     }
 
-    public void Activate()
+    public void Activate() // 함수 Active 선언 및 정의
     {
-        StartCoroutine(SpawnEnemy());
+        StartCoroutine(SpawnEnemy()); //StartCoruntine 함수를 통한 SpawnEnemy 코루틴을 작동시킨다
     }
 
-    public void DeActivate()
+    public void DeActivate() // 함수 DeActive 선언 및 정의
     {
-        StopCoroutine(SpawnEnemy());
+        StopCoroutine(SpawnEnemy()); // StopCoruntine 함수를 통해 작동중인 코루틴 SpawnEnemy를 중단시킴
     }
 
-    IEnumerator SpawnEnemy()
+    IEnumerator SpawnEnemy() // SpawnEnemy 코루틴을 정의 및 선언
     {
-        while (_bCanSpawn)
+        while (_bCanSpawn) // _bCanSpawn 논리형이 True일 때만 작동하는 while 반복문,  게임 내에서 지속적으로 Enemy를 스폰해주기 위해 반복하는 역활을 한다
         {
-            yield return new WaitForSeconds(SpawnCycleTime);
+            yield return new WaitForSeconds(SpawnCycleTime); // WaitForSecond를 반환하여 가로 안에 있는 시간(초)가 지나면 해당 구문 및에 부분을 실행시킨다
 
-            GameObject EnemyInst = Instantiate(EnemyPrefab, SpawnPosition.position, Quaternion.identity);
-            Enemy EnemyCom = EnemyInst.GetComponent<Enemy>();
-            if (EnemyCom)
+            GameObject EnemyInst = Instantiate(EnemyPrefab, SpawnPosition.position, Quaternion.identity); // 게임 오브젝트 타입 EnemyInst를 선언 및 정의하여 Instatie함수를 통해 가로 안에있는 EnemyPrefab 오브젝트를
+                                                                                                          // SpawnPosition의 위치에 동적으로 생성한다
+
+            Enemy EnemyCom = EnemyInst.GetComponent<Enemy>(); // Enemy 클래스 타입의 EnemyCom을 선언 및 정의하여 게임오브젝트 EnemyInst에서 Enemy 컴퍼넌트를 찾는다
+
+            if (EnemyCom) // 만약 EnemyCom이 존재한다면 if문 안에 있는 코드를 실행
             {
-                EnemyCom.WayPoints = WayPoints;
+                EnemyCom.WayPoints = WayPoints;// GameObject EnemyPrefab안에 있는 Enemy 클래스 WayPoints 배열 변수에 EnemySpawner 클래스의 WayPoints 배열 값을 집어넣는다
             }
         }
     }
